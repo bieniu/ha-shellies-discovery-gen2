c5ddcb9bfcc45d3a237cc5c2939d602d0b8665a3
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-10-26 16:22:31 +0200

    Fixes

--- python_scripts/shellies_discovery_gen2.py
+++ python_scripts/shellies_discovery_gen2.py
@@ -152,7 +152,6 @@ STATE_CLASS_MEASUREMENT = "measurement"
 STATE_CLASS_TOTAL_INCREASING = "total_increasing"
 
 TOPIC_COVER = "~status/cover:{cover}"
-TOPIC_DEVICE_INFO_RPC = "~device-info/rpc"
 TOPIC_HUMIDITY = "~status/humidity:0"
 TOPIC_INPUT = "~status/input:{relay}"
 TOPIC_ONLINE = "~online"
@@ -172,21 +171,17 @@ TPL_CURRENT = "{{value_json.current|round(1)}}"
 TPL_ENERGY = "{{value_json.aenergy.total|round(2)}}"
 TPL_ETH_IP = "{{value_json.result.eth.ip}}"
 TPL_EXTERNAL_POWER = "{%if value_json.external.present%}ON{%else%}OFF{%endif%}"
-TPL_FIRMWARE_STABLE = (
-    "{{value_json.result.sys.available_updates.get(^stable^,{^version^:null})}}"
-)
+TPL_FIRMWARE_STABLE = "{%if value_json.result.sys.available_updates.stable is defined%}{{value_json.result.sys.available_updates.stable}}{%else%}{{value_json.result.sys.installed_version}}{%endif%}"
 TPL_FIRMWARE_STABLE_INDEPENDENT = (
     "{%if value_json.available_updates.stable is defined%}ON{%else%}OFF{%endif%}"
 )
-TPL_FIRMWARE_STABLE_ATTRS = (
-    "{{value_json.result.sys.available_updates.get(^stable^,{})|to_json}}"
-)
+TPL_FIRMWARE_STABLE_ATTRS = "{{value_json.available_updates.get(^stable^,{})|to_json}}"
 TPL_FIRMWARE_STABLE_ATTRS_INDEPENDENT = (
     "{{value_json.available_updates.get(^stable^,{})|to_json}}"
 )
 TPL_HUMIDITY = "{{value_json.rh|round(1)}}"
 TPL_INPUT = "{%if value_json.state%}ON{%else%}OFF{%endif%}"
-TPL_INSTALLED_FIRMWARE = "{{value_json.result.ver}}"
+TPL_INSTALLED_FIRMWARE = "{{value_json.result.sys.installed_version}}"
 TPL_MQTT_CONNECTED = (
     "{%if value_json.result.mqtt.connected%}online{%else%}offline{%endif%}"
 )
@@ -520,12 +515,12 @@ DESCRIPTION_UPDATE_FIRMWARE = {
     KEY_DEVICE_CLASS: "firmware",
     KEY_ENABLED_BY_DEFAULT: True,
     KEY_ENTITY_CATEGORY: ENTITY_CATEGORY_DIAGNOSTIC,
-    KEY_LATEST_VERSION_TEMPLATE: TPL_INSTALLED_FIRMWARE,
-    KEY_LATEST_VERSION_TOPIC: TOPIC_DEVICE_INFO_RPC,
+    KEY_LATEST_VERSION_TEMPLATE: TPL_FIRMWARE_STABLE,
+    KEY_LATEST_VERSION_TOPIC: TOPIC_STATUS_RPC,
     KEY_NAME: "Firmware",
     KEY_PAYLOAD_INSTALL: "{{^id^:1,^src^:^{device_id}^,^method^:^Shelly.Update^,^params^:{{^stage^:^stable^}}}}",
     KEY_STATE_TOPIC: TOPIC_STATUS_RPC,
-    KEY_VALUE_TEMPLATE: TPL_FIRMWARE_STABLE,
+    KEY_VALUE_TEMPLATE: TPL_INSTALLED_FIRMWARE,
 }
 
 
@@ -551,6 +546,9 @@ SUPPORTED_MODELS = {
             SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
         },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
@@ -635,6 +633,9 @@ SUPPORTED_MODELS = {
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
         },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
     MODEL_PLUS_HT: {
@@ -677,6 +678,9 @@ SUPPORTED_MODELS = {
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
         },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
     MODEL_PLUS_PLUG_US: {
@@ -709,6 +713,9 @@ SUPPORTED_MODELS = {
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
         },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20220211,
     },
     MODEL_PRO_1: {
@@ -733,6 +740,9 @@ SUPPORTED_MODELS = {
             SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
         },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
@@ -771,6 +781,9 @@ SUPPORTED_MODELS = {
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
         },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
     MODEL_PRO_2: {
@@ -795,6 +808,9 @@ SUPPORTED_MODELS = {
             SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
         },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
@@ -842,6 +858,9 @@ SUPPORTED_MODELS = {
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
         },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
     MODEL_PRO_3: {
@@ -867,6 +886,9 @@ SUPPORTED_MODELS = {
             SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
         },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
@@ -904,6 +926,9 @@ SUPPORTED_MODELS = {
             SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
             SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
             SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
         },
         ATTR_MIN_FIRMWARE_DATE: 20220308,
     },
@@ -1255,6 +1280,10 @@ def get_update(update, description):
         KEY_NAME: f"{device_name} {description[KEY_NAME]}",
         KEY_ENABLED_BY_DEFAULT: str(description[KEY_ENABLED_BY_DEFAULT]).lower(),
         KEY_UNIQUE_ID: f"{device_id}-{update}".lower(),
+        KEY_STATE_TOPIC: description[KEY_STATE_TOPIC],
+        KEY_VALUE_TEMPLATE: description[KEY_VALUE_TEMPLATE],
+        KEY_LATEST_VERSION_TEMPLATE: description[KEY_LATEST_VERSION_TEMPLATE],
+        KEY_LATEST_VERSION_TOPIC: description[KEY_LATEST_VERSION_TOPIC],
         KEY_QOS: qos,
         KEY_AVAILABILITY: availability,
         KEY_DEVICE: device_info,
