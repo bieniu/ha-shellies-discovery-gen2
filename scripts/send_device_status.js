fa3deef372582612829bec9447a29d5de7938441
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-10-27 08:48:54 +0200

    Update script

--- scripts/send_device_status.js
+++ scripts/send_device_status.js
@@ -1,18 +1,22 @@
 let topic_prefix = null;
 let installed_version = null;
+let model = null;
 
 Shelly.call("MQTT.GetConfig", {}, function (config) {
     topic_prefix = config.topic_prefix;
 });
 
 function SendDeviceStatus() {
-    Shelly.call("Shelly.GetDeviceInfo", {}, function (device_info) {
-        installed_version = device_info.ver;
-    });
+    let _device_info = Shelly.getDeviceInfo();
+    installed_version = _device_info.ver;
+    model = _device_info.model;
     Shelly.call("Shelly.GetStatus", {}, function (status) {
         status.sys.installed_version = installed_version;
+        status.sys.model = model;
         MQTT.publish(topic_prefix + "/status/rpc", JSON.stringify({ "result": status }));
     });
 }
 
-let UpdateTimer = Timer.set(300000, true, SendDeviceStatus);
+
+MQTT.setConnectHandler(SendDeviceStatus)
+let UpdateTimer = Timer.set(30000, true, SendDeviceStatus);
