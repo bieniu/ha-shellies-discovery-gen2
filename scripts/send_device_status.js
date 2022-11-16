07ca079d6acf64aa8a9fa68ff23081e986fb6679
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-11-16 17:05:28 +0100

    Fix `update device status` script (#113)
    * Script fix
    
    * Add missing ;

--- scripts/send_device_status.js
+++ scripts/send_device_status.js
@@ -10,11 +10,10 @@ function SendDeviceStatus() {
     installed_version = _device_info.ver;
     Shelly.call("Shelly.GetStatus", {}, function (status) {
         status.sys.installed_version = installed_version;
-        status.sys.model = model;
         MQTT.publish(topic_prefix + "/status/rpc", JSON.stringify(status));
     });
-}
+};
 
 
-MQTT.setConnectHandler(SendDeviceStatus)
+MQTT.setConnectHandler(SendDeviceStatus);
 let UpdateTimer = Timer.set(30000, true, SendDeviceStatus);
