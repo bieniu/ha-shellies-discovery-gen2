19c720aa0a6c9ea67b79e49dc4d8019e95a9de8f
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-12-23 12:15:00 +0100

    Bump version

--- python_scripts/shellies_discovery_gen2.py
+++ python_scripts/shellies_discovery_gen2.py
@@ -1,5 +1,5 @@
 """This script adds MQTT discovery support for Shellies Gen2 devices."""
-VERSION = "2.23.0"
+VERSION = "2.24.0"
 
 ATTR_BATTERY_POWERED = "battery_powered"
 ATTR_BINARY_SENSORS = "binary_sensors"
