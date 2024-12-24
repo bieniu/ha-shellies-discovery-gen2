e30f25ce6e22c3c7442981ff77a23c2b2fc434f5
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-12-24 10:43:57 +0100

    Remove off mode

--- python_scripts/shellies_discovery_gen2.py
+++ python_scripts/shellies_discovery_gen2.py
@@ -3563,13 +3563,7 @@ def get_blu_climate(thermostat_id: str, description) -> tuple:
         KEY_TEMP_STEP: description[ATTR_TEMPERATURE_STEP],
         KEY_MIN_TEMP: description[ATTR_TEMPERATURE_MIN],
         KEY_MAX_TEMP: description[ATTR_TEMPERATURE_MAX],
-        KEY_MODES: ["off", "heat"],
-        KEY_MODE_STATE_TOPIC: TOPIC_STATUS_BLU_TRV.format(id=thermostat_id),
-        KEY_MODE_STATE_TEMPLATE: TPL_BLU_THERMOSTAT_MODE,
-        KEY_MODE_COMMAND_TOPIC: TOPIC_RPC,
-        KEY_MODE_COMMAND_TEMPLATE: TPL_SET_BLU_THERMOSTAT_MODE.format(
-            source=source_topic, thermostat=thermostat_id
-        ),
+        KEY_MODES: ["heat"],
         KEY_AVAILABILITY: availability,
         KEY_UNIQUE_ID: f"{device_id}-{thermostat_id}".lower(),
         KEY_QOS: qos,
