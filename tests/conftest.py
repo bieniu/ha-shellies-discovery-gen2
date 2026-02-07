b3215412be92fef38ec3cac65b165d13b93aca74
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-02-07 17:03:09 +0100

    Use syrupy

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+"""Set up some common test helper things."""
+
+from pathlib import Path
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+from syrupy.extensions.amber import AmberSnapshotExtension
+from syrupy.location import PyTestLocation
+
+
+@pytest.fixture
+def snapshot(snapshot: SnapshotAssertion) -> SnapshotAssertion:
+    """Return snapshot assertion fixture."""
+    return snapshot.use_extension(SnapshotExtension)
+
+
+class SnapshotExtension(AmberSnapshotExtension):
+    """Extension for Syrupy."""
+
+    @classmethod
+    def dirname(cls, *, test_location: PyTestLocation) -> str:
+        """Return the directory for the snapshot files."""
+        test_dir = Path(test_location.filepath).parent
+        return str(test_dir.joinpath("snapshots"))
