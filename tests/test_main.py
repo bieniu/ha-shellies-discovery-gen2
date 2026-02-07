858871dbb3e457d30f3576352971bb08b3d34e60
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-02-07 23:56:53 +0100

    Add Plug PM Gen3 support

--- tests/test_main.py
+++ tests/test_main.py
@@ -172,6 +172,23 @@ def test_shelly_plug_m_gen3(snapshot: SnapshotAssertion) -> None:
         if payload := hass.services.call.call_args_list[call][0][2].get("payload"):
             assert json.loads(payload) == snapshot
 
+def test_shelly_plug_pm_gen3(snapshot: SnapshotAssertion) -> None:
+    """Test for Shelly Plug PM Gen3."""
+    logger = Mock()
+    hass = Mock()
+    hass.services = Mock()
+    fixture_path = FIXTURES_PATH / "shelly_plug_pm_gen3.json"
+    data = json.loads(fixture_path.read_text())
+
+    result = run_script(logger=logger, data=data, hass=hass)
+
+    assert result.config_data
+    assert hass.services.call.called
+
+    for call in range(len(hass.services.call.call_args_list)):
+        if payload := hass.services.call.call_args_list[call][0][2].get("payload"):
+            assert json.loads(payload) == snapshot
+
 def test_shelly_blu_rc_button_4_zb(snapshot: SnapshotAssertion) -> None:
     """Test for Shelly BLU RC Button 4 ZB."""
     logger = Mock()
