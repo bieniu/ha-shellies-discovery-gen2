6b6c9abc8be405b5c9e5202947111151facb6cd2
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-04-13 22:47:52 +0200

    Support BLU H&T ZB (#808)

--- tests/test_main.py
+++ tests/test_main.py
@@ -157,6 +157,7 @@ def test_mqtt_prefix_with_space() -> None:
         "shelly_plug_m_gen3",
         "shelly_plug_pm_gen3",
         "shelly_blu_rc_button_4_zb",
+        "shelly_blu_ht_zb",
         "shelly_blu_ht_display_zb",
         "shelly_flood_s_gen4",
         "shelly_plus_1pm",
